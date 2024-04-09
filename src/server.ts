85cdf2403942cdd826848b3f3b6e373b7343c881
Cláudio Melgaço <claudio19a@hotmail.com>
2024-04-09 16:19:55 -0300

    Alterando aquivo server

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import { routes } from './routes';
+
 const portHost = process.env.PORT || 3030;
 
 const app = Fastify({ logger: true });
 
 const start = async () => {
-
   await app.register(cors);
   await app.register(routes);
 
   try {
-    await app.listen({ port: portHost});
+    await app.listen(portHost);
   } catch (err) {
-    process.exite(1);
+    console.error('Erro ao iniciar o servidor:', err);
+    process.exit(1);
   }
 }
 
