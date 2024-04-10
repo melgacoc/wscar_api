151b76a71620aa85545a6613df8cb9e6e794c752
Cláudio Melgaço <claudio19a@hotmail.com>
2024-04-10 08:59:55 -0300

    Adequando rotas

--- src/routes.ts
+++ src/routes.ts
@@ -5,36 +5,27 @@ import CreateCarController from './controller/newCarController';
 import CreateCarByBrandController from './controller/newCarByBrandController';
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginAsync) {
-
-    fastify.get("/getCars", async (req: FastifyRequest, reply: FastifyReply) => {
-        const getCarsController = new GetCarsController();
-
-        const response = await getCarsController.execute(req, reply);
-
+    fastify.get("/getCarsByBrand", async (req: FastifyRequest, reply: FastifyReply) => {
+        const getCarsByBrandController = new GetCarsByBrandController();
+        const response = await getCarsByBrandController.execute(req, reply);
         return response;
     });
 
-    fastify.get("/getCarsByBrand", async (req: FastifyRequest, reply: FastifyReply) => {
-        const getCarsByBrandController = new GetCarsByBrandController();
-
-        const response = await getCarsByBrandController.execute(req, reply);
-
+    fastify.get("/getCars", async (req: FastifyRequest, reply: FastifyReply) => {
+        const getCarsController = new GetCarsController();
+        const response = await getCarsController.execute(req, reply);
         return response;
     });
 
     fastify.post("/newCar", async (req: FastifyRequest, reply: FastifyReply) => {
         const createCarController = new CreateCarController();
-
         const response = await createCarController.execute(req, reply);
-
         return response;
     });
 
     fastify.post("/newCarByBrand", async (req: FastifyRequest, reply: FastifyReply) => {
         const createCarByBrandController = new CreateCarByBrandController();
-
         const response = await createCarByBrandController.execute(req, reply);
-
         return response;
     });
 }
